9c694a8af68cfcd822c37f35b19dd71efb674cf8
Niall Robinson <niall.robinson@informaticslab.co.uk>
2015-12-23 10:50:06 +0000

    tweaks

--- LabbyBot.py
+++ LabbyBot.py
@@ -1,16 +1,18 @@
 
 import time
 from BrickPi import *
-import EV3peripherals as ev3
+from EV3peripherals.Track import Track
+from EV3peripherals.BiTrack import BiTrack
+from EV3peripherals.InfraredSensor import IRSensor
 
 class LabbyBot:
 
     def __init__(self, **kwargs):
         BrickPiSetup()
-        self.leftTrack = ev3.Track.Track(kwargs['leftTrackPort'])
-        self.rightTrack = ev3.Track.Track(kwargs['rightTrackPort'])
-        self.biTrack = ev3.BiTrack.BiTrack(self.leftTrack, self.rightTrack)
-        self.irSensor = ev3.InfraredSensor.IRSensor(kwargs['irSensorPort'])
+        self.leftTrack = Track(kwargs['leftTrackPort'])
+        self.rightTrack = Track(kwargs['rightTrackPort'])
+        self.biTrack = BiTrack(self.leftTrack, self.rightTrack)
+        self.irSensor = IRSensor(kwargs['irSensorPort'])
         BrickPiSetupSensors()
 
     def bringToLife(self):
@@ -27,10 +29,10 @@ class LabbyBot:
             time.sleep(.1)
 
 
-    def edgeDetected(self, limit=100):
+    def edgeDetected(self, limit=15):
         return self.irSensor.getValue() >= limit
 
 
 if __name__=='__main__':
-    myRobot = LabbyBot(leftTrackPort='PORT_D',rightTrackPort='PORT_A',irSensorPort='PORT_1')
+    myRobot = LabbyBot(leftTrackPort=PORT_D, rightTrackPort=PORT_A, irSensorPort=PORT_1)
     myRobot.bringToLife()
